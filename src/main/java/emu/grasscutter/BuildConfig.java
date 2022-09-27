2c33bdd3cf2395c8830f8083996665999b27ce2b
Ayame <igortfreitas14@gmail.com>
2022-09-27 16:16:11 -0300

    banners 3.1 update

--- src/main/java/emu/grasscutter/BuildConfig.java
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -2,5 +2,5 @@ package emu.grasscutter;
 
 public final class BuildConfig {
     public static final String VERSION = "1.3.1-dev";
-    public static final String GIT_HASH = "GIT_NOT_FOUND";
+    public static final String GIT_HASH = "28ec94cd2";
 }
